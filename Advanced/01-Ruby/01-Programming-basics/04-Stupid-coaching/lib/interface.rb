73ee6f93bc44ed249d1281eaf0aedabd23eb1d5f
pierrogoffard <pierre.olivier.goffard@gmail.com>
2014-09-09 21:38:47 +0200

    Done

--- Advanced/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
+++ Advanced/01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,11 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
+answer=" "
+
+while !answer.empty?
+message=gets.strip
+answer=coach_answer(message)
+puts answer
+end
+
 
