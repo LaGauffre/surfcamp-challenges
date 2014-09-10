25a6e165dd3ed3c5497ba9e28ca019c16d37124a
pierrogoffard <pierre.olivier.goffard@gmail.com>
2014-09-10 17:03:27 +0200

    Grid letters random

--- Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
+++ Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
@@ -1,8 +1,15 @@
 require 'open-uri'
 require 'json'
 
+
+
 def generate_grid(grid_size)
   #TODO: generate random grid of letters
+grid=[]
+for i in 0..8
+  grid << ("a".."z").map{|letter| letter}.sample
+end
+grid
 end
 
 def run_game(attempt, grid, start_time, end_time)
